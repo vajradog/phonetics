afe66ba14b2c1cc714a0816475d99fe9e00a0a95
Thupten Chakrishar <vajradog@gmail.com>
2015-09-04 08:55:16 -0400

    view changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150623173418) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "phonetics", force: :cascade do |t|
     t.string   "word"
     t.datetime "created_at"
